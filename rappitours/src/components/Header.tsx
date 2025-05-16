1f57c15e44294ebd8385f98598033cd07623197c
nano <df@proconsis.ch>
2025-05-16 09:59:13 +0200

    adjusted css by a bit

--- rappitours/src/components/Header.tsx
+++ rappitours/src/components/Header.tsx
@@ -1,9 +1,11 @@
 import {ModeToggle} from "@/components/mode-toggle.tsx";
+import logo from '/default_logo.svg';
 
 function Header() {
   return (
     <header className="flex items-center justify-between p-4 bg-secondary text-primary">
-      <h1 className="text-2xl font-bold">Rappitours</h1>
+        <img src={logo} alt="Logo" style={{ width: '50px', height: '50px' }} />
+        <h1 className="text-2xl font-bold">Rappitours</h1>
       <ModeToggle/>
     </header>
   );
